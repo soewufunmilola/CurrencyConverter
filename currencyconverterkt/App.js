d326cb9c62f73ad9c163cba1b82402ffd1f82feb
soewufunmilola <soewufunmilola@gmail.com>
2017-10-24 12:30:54 +0100

    I have Put the header "CURRRENCY CONVERTER"

--- currencyconverterkt/App.js
+++ currencyconverterkt/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={viewStyles.container}>
+
+            <View style= {viewStyles.headerArea}>
+              <Text style={viewStyles.headertextStyle}> CURRENCY CONVERTER </Text>
+            </View>
+
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+const viewStyles =
+(
+  {
+  container:
+    {
+      flex: 1,
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
+      paddingTop: 29
+    },
+
+  headertextStyle:
+    {
+      color: 'black',
+      fontSize: 30,
+      fontWeight: '300',
+
+    },
+
+  textStyle:
+    {
+      color: 'red',
+      fontSize: 35,
+    },
+/*
+the style for the header area of the app
+*/
+   headerArea:
+   {
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    width: '100%',
+    height: '10%',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+   }
+
 });
